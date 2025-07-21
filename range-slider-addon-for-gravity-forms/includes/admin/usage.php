70d564e29e18a0b49d4f02891899330372e98966
codeprintagency <74283129+codeprintagency@users.noreply.github.com>
2025-07-21 14:56:30 -0500

    unzipgit

---
+++ range-slider-addon-for-gravity-forms/includes/admin/usage.php
@@ -0,0 +1,48 @@
+<div id="usage" class="content-usage tab_item">
+    <div class="content_heading">
+        <h2><?php esc_html_e('Basic Usage', 'range-slider-addon-for-gravity-forms'); ?></h2>
+        <p><?php esc_html_e('A Beginner\'s Guide to Efficiently Using the Plugin', 'range-slider-addon-for-gravity-forms'); ?></p>
+    </div>
+
+    <section class="section_full background_free">
+        <div class="col_description">
+            <h2><?php esc_html_e('Basic Usage (Free version)', 'range-slider-addon-for-gravity-forms'); ?></h2>
+            <p><?php esc_html_e('After installing the plugin, you\'ll find the range slider field under the Standard tab in Gravity Forms. Simply click on the range slider field or drag and drop it into the Gravity Forms field panel to add the slider to your form.', 'range-slider-addon-for-gravity-forms'); ?></p>
+            <p><?php esc_html_e('Now, configure it according to your requirements, as shown in the image on the right.', 'range-slider-addon-for-gravity-forms'); ?></p>
+        </div>
+        <div class="col_image">
+            <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/free.png'); ?>">
+        </div>
+    </section>
+
+    <div class="usage_wrap">
+        <section class="section_full background_pro">
+            <div class="col_description">
+                <h2><?php esc_html_e('Basic Usage Number Slider (Pro)', 'range-slider-addon-for-gravity-forms'); ?></h2>
+                <p><?php esc_html_e('After installing the plugin, you\'ll find the range slider field under the Advanced tab in Gravity Forms. Simply click on the range slider field or drag and drop it into the Gravity Forms field panel to add the slider to your form.', 'range-slider-addon-for-gravity-forms'); ?></p>
+                <p><?php esc_html_e('Now, configure it according to your requirements, as shown in the image on the right.', 'range-slider-addon-for-gravity-forms'); ?></p>
+                <ol>
+                    <li>
+                        <?php esc_html_e('In the slider type settings, you can choose between a single or double slider.', 'range-slider-addon-for-gravity-forms'); ?>
+                    </li>
+                    <li><?php esc_html_e('Set the minimum and maximum values for the range slider, along with the step increment. You can also define default minimum and maximum values that apply when the slider loads.', 'range-slider-addon-for-gravity-forms'); ?></li>
+                </ol>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/number_slider.png'); ?>">
+            </div>
+        </section>
+        <section class="section_full background_pro">
+            <div class="col_description">
+                <h2><?php esc_html_e('Basic Usage Text Slider (Pro)', 'range-slider-addon-for-gravity-forms'); ?></h2>
+                <p><?php esc_html_e('After installing the plugin, you\'ll find the range slider field under the Advanced tab in Gravity Forms. Simply click on the range slider field or drag and drop it into the Gravity Forms field panel to add the slider to your form.', 'range-slider-addon-for-gravity-forms'); ?></p>
+                <p><?php esc_html_e('Now, configure it according to your requirements, as shown in the image on the right.', 'range-slider-addon-for-gravity-forms'); ?></p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/text-slider.png'); ?>">
+            </div>
+        </section>
+    </div>
+
+
+</div>
