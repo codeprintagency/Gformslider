d866b4248852a73aab88537d5ab21725af87ce89
codeprintagency <74283129+codeprintagency@users.noreply.github.com>
2025-07-21 14:56:28 -0500

    unzipgit

---
+++ range-slider-addon-for-gravity-forms/includes/admin/introduction.php
@@ -0,0 +1,130 @@
+<div id="introduction" class="content-introduction active tab_item">
+    <section class="section_full background_free full_width">
+        <div class="col_description">
+            <h2><?php esc_html_e('Quick Overview', 'range-slider-addon-for-gravity-forms'); ?></h2>
+            <p><?php esc_html_e('Range Slider for Gravity Forms is a simple and lightweight plugin that allows you to add a range slider field to your Gravity Forms. It is easy to use, smooth, lightweight, customizable range slider and unrivaled performance on both mobile and desktop platforms across all major browsers.', 'range-slider-addon-for-gravity-forms'); ?></p>
+        </div>
+    </section>
+
+    <div class="content_heading">
+        <h2><?php esc_html_e('Never miss a valuable features', 'range-slider-addon-for-gravity-forms'); ?></h2>
+        <p><?php esc_html_e('Let\'s explore the awesome features of the plugin', 'range-slider-addon-for-gravity-forms'); ?></p>
+    </div>
+
+    <div class="section_wrap">
+        <!-- Slider Variation -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Slider Variation', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('This plugin offers a variety of type options to suit your needs. Currently, it includes two types of sliders: the number slider and the text slider.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/slider-variation.png'); ?>">
+            </div>
+        </section>
+        <!-- Single and dual handle slider -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Single and dual handle slider', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('You can use two handles on the range slider for both number and text sliders. In the free version, only one handle is available, while the pro version allows you to use two handles along with additional features.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/dual-slider.png'); ?>">
+            </div>
+        </section>
+        <!-- Default min & max value -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Default/Start min & max value', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('The range slider offers many useful options. You can set the step, minimum, start, and end values, as well as the default minimum and maximum values. Note that maximum values are required for using the double handle.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/min-max-default-value.png'); ?>">
+            </div>
+        </section>
+        <!-- Prefix & postfix -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Prefix & postfix', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('Prefix and postfix are essential for the range slider. You can use any text or symbol for both, including spaces. The prefix is available in the free version, while the postfix is included in the pro version.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/prefix.png'); ?>">
+            </div>
+        </section>
+        <!-- Tooltip & Pips -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Tooltips & Pips', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('Tooltips and pips enhance the slider\'s usability, making it easier for users to select data. The tooltips is available in the free version, while the pips is included in the pro version.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/tooltips.png'); ?>">
+            </div>
+        </section>
+        <!-- RTL support -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('RTL support', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('RTL (Right to Left) is a key feature for certain languages and countries. You can enable RTL using a checkbox, which is available in the pro version.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/rtl.png'); ?>">
+            </div>
+        </section>
+        <!-- Calculation Support -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Calculation support', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('This slider supports calculations in the number and product fields of Gravity Forms. You can use it like any other calculation field merge tag. It is available in both the free and pro versions.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/calculation.png'); ?>">
+            </div>
+        </section>
+        <!-- Connect With Number Field -->
+        <section class="section_half">
+            <div class="col_description">
+                <h3><?php esc_html_e('Connect With Number Field', 'range-slider-addon-for-gravity-forms'); ?></h3>
+                <p>
+                    <?php
+                    esc_html_e('You can seamlessly link the range slider handle with the number field. The number field settings provide an option to configure this, enabling a two-way integration.', 'range-slider-addon-for-gravity-forms');
+                    ?>
+                </p>
+            </div>
+            <div class="col_image">
+                <img src="<?php echo esc_url(GF_NU_RANGE_SLIDER_URL . '/assets/images/cwnf.png'); ?>">
+            </div>
+        </section>
+    </div>
+
+
+</div>
